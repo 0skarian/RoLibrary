a2e955c49dd69412affee3206b3882847748682d
0skarian <47034686+0skarian@users.noreply.github.com>
2019-07-28 20:41:19 +0200

    Create Featured.lua

---
+++ Marketplace/Featured.lua
@@ -0,0 +1,8 @@
+local FeaturedFiles = {
+  {"Audio","284392291"},
+  {"Image","3217037769"}
+}
+
+local FeaturedMessage = {"Featured Tab","Let's get that idea machine rolling!"}
+
+return FeaturedFiles, FeaturedMessage
